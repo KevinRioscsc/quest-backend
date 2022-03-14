85476d8668f083575456915e3ce8eac38adb102b
Kevin Rios <rioskevin89@gmail.com>
2022-03-14 16:30:00 -0700

    yes

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const app = express();
+const knex = require('knex')
+const cors = require('cors');
+
+const db = knex({
+    client: 'pg',
+    connection:{
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false
+        }
+    }
+});
+/*knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'Kevthebest12@',
+      database : 'trello-clone',
+      idleTimeoutMillis: 0,
+      connectionTimeoutMillis: 0,
+    }
+  });*/
+
+  
+
+
+
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/createList', (req, res) => {
+    const {pid, title} = req.body;
+
+    db('list').insert({
+        pid:pid,
+        title:title,
+    }).returning('lid').then(resp => res.json(resp[0]))
+    
+})
+app.post('/createProject', (req, res) => {
+    const {uid, title, url} = req.body;
+
+    db('projects').insert({
+        uid:uid,
+        title:title,
+        url:url
+    }).returning('pid').then(resp => res.json(resp[0]))
+    
+})
+app.post('/createCard', (req, res) => {
+    const {lid, title} = req.body;
+
+    db('card').insert({
+        lid:lid,
+        title:title,
+    }).then(resp => res.json('success'))
+    
+})
+
+app.post('/getProjects', (req, res) => {
+    const {uid} = req.body
+    db('projects').where('uid', '=', uid).then(response => res.json(response))
+
+})
+app.post('/getList', (req, res) => {
+    const {pid} = req.body
+    db('list').where('pid', '=', pid).then(response => res.json(response))
+
+})
+app.post('/getCard', (req, res) => {
+    const {lid} = req.body
+    db('card').where('lid', '=', lid).then(response => res.json(response))
+
+})
+app.post('/delCard', (req, res) => {
+    const {cid} = req.body
+    db('card')
+  .where({ cid: cid })
+  .del().then(resp => res.json('success'))
+
+})
+app.post('/getProject', (req, res) => {
+    const {pid} = req.body
+    db('projects').where('pid', '=', pid).then(response => res.json(response))
+
+})
+app.get('/', (req, res) =>{res.send('it is working!')})
+
+
+
+app.listen(process.env.PORT || 3001, ()=>{
+    console.log("We are listening")
+});
